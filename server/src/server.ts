f232c10b1fd1db0ee5957eb1b5fd8101a2cd8cd7
gasparcl <gasparcl.dev@gmail.com>
2023-01-17 18:16:46 -0300

    Feat: files update - 42:30

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,32 @@
 import Fastify from "fastify"
+import cors from "@fastify/cors"
+import { PrismaClient } from "@prisma/client"
 
+const DEFAULT_LISTENING_PORT = 3333
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get("/", () => {
-    return "Hello World"
+app.register(cors)
+
+app.get("/", async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: "Beber",
+            },
+        },
+    })
+
+    // para retornar todos os registros de habit, bastaria não passar nenhum parâmetro para o findMany:
+    // const habits = await prisma.habit.findMany()
+
+    return habits
 })
 
 app.listen({
-    port: 3333,
-})
+    port: DEFAULT_LISTENING_PORT,
+}).then(() =>
+    console.log(
+        `The server is running on http://localhost:${DEFAULT_LISTENING_PORT}/`
+    )
+)
