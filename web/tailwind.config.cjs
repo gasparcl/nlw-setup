5ab8ea782cc09a61fd05a45c52c6f11c3543567f
gasparcl <gasparcl.dev@gmail.com>
2023-01-20 15:26:35 -0300

    style: Index styled / colors and gridTemplateRows added totailwind

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
     content: ["./src/**/*.tsx", "./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: {
+                    main: "#09090A",
+                },
+            },
+            gridTemplateRows: {
+                7: "repeat(7, minmax(0, 1fr))",
+            },
+        },
     },
     plugins: [],
 }
